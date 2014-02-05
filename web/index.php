b062087653222832b0ee481c96a94a5983319a38
Vasilij <Vasilij@Chronos>
2014-02-05 04:34:06 +0200

    UPDATE: slim php and twig added and configured

--- web/index.php
+++ web/index.php
@@ -1,2 +1,21 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php';
+$app = new \Slim\Slim(array(
+    'debug' => true,
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => '../templates',
+));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension(),
+);
+
+$app->get('/', function () use ($app) {
+    $app->render('index.html.twig');
+})->name('home');
+
+$app->run();
